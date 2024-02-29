e04c835a5c4efa60ec3f62fc37e6e19373154dad
ankitakhot <ankitakhot6721@gmail.com>
2024-02-29 19:33:39 +0000

    intial commit

---
+++ UserManagement.Web/Controllers/UsersController.cs
@@ -0,0 +1,123 @@
+﻿using System.Linq;
+using UserManagement.Models;
+using UserManagement.Services.Domain.Interfaces;
+using UserManagement.Web.Models.Users;
+
+namespace UserManagement.WebMS.Controllers;
+
+[Route("users")]
+public class UsersController : Controller
+{
+    private readonly IUserService _userService;
+    public UsersController(IUserService userService) => _userService = userService;
+
+    [HttpGet]
+    public ViewResult List()
+    {
+        var items = _userService.GetAll().Select(p => new UserListItemViewModel
+        {
+            Id = p.Id,
+            Forename = p.Forename,
+            Surname = p.Surname,
+            Email = p.Email,
+            IsActive = p.IsActive
+        });
+
+        var model = new UserListViewModel
+        {
+            Items = items.ToList()
+        };
+
+        return View(model);
+    }
+
+    [Route("details/{id:int}")]
+    public ActionResult Details(int id)
+    {
+        //delete_by_id(id);
+
+        // Return desired view
+        var res = _userService.GetUser(id);
+        var model = new UserListItemViewModel
+        {
+            Id = res.Id,
+            Forename = res.Forename,
+            Surname = res.Surname,
+            Email = res.Email,
+            IsActive = res.IsActive
+        };
+        return View(model);
+    }
+
+    [Route("edit/{id:int}")]
+    public ActionResult Edit(int id)
+    {
+        var res = _userService.GetUser(id);
+        var model = new UserListItemViewModel
+        {
+            Id = res.Id,
+            Forename = res.Forename,
+            Surname = res.Surname,
+            Email = res.Email,
+            IsActive = res.IsActive
+        };
+        return View(model);
+    }
+
+    [HttpPost]
+    [Route("confirmedtoedit")]
+    public ActionResult ConfirmedToEdit(User res)
+    {
+        if (ModelState.IsValid)
+        {
+            var model = new UserListItemViewModel
+            {
+                Id = res.Id,
+                Forename = res.Forename,
+                Surname = res.Surname,
+                Email = res.Email,
+                IsActive = res.IsActive
+            };
+            _userService.Update(res);
+            TempData["message"] = "User edited successfully";
+        }
+        return RedirectToAction("List");
+    }
+
+
+    [Route("delete/{id:int}")]
+    public ActionResult Delete(int id)
+    {
+
+        var deletedUser = _userService.Delete(id);
+        TempData["message"] = "User deleted successfully";
+        return RedirectToAction("List");
+    }
+
+    [Route("create")]
+    public ActionResult Create(int id)
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("confirmedtoadd")]
+    public ActionResult ConfirmedToAdd(User res)
+    {
+        if(ModelState.IsValid)
+        {
+            var model = new UserListItemViewModel
+            {
+                Id = res.Id,
+                Forename = res.Forename,
+                Surname = res.Surname,
+                Email = res.Email,
+                IsActive = res.IsActive
+            };
+            _userService.Add(res);
+            TempData["message"] = "User added successfully";
+        }
+        return RedirectToAction("List");
+
+    }
+}
---
+++ UserManagement.Web/Models/Users/UserListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Web.Models.Users;
+
+public class UserListViewModel
+{
+    public List<UserListItemViewModel> Items { get; set; } = new();
+}
+
+public class UserListItemViewModel
+{
+    public long Id { get; set; }
+    public string? Forename { get; set; }
+    public string? Surname { get; set; }
+    [Required]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email format")]
+    public string? Email { get; set; }
+    public bool IsActive { get; set; }
+}
